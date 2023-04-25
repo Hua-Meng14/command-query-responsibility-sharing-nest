cd62cd90c3391f6864bf2d8f212dd0739fcef52a
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-04-25 14:14:43 +0700

    add Person entity , module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from './entities/person/person';
+import { PersonModule } from './person/person.module';
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      database: 'postgres',
+      entities: [Person]
+    }),
+    PersonModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
