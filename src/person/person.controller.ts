280562087770d4d393af1df6f0275dc751e39e68
Hua-Meng14 <lim.huameng19@kit.edu.kh>
2023-04-25 15:34:24 +0700

    add post request endpoint for commands

--- src/person/person.controller.ts
+++ src/person/person.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import { Body, Controller, Get, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { GetPersonQuery } from './queries/impl/get-person.query/get-person.query';
+import { SavePersonCommand } from './commands/impl/save-person.command/save-person.command';
 
 
 @Controller('person')
 export class PersonController {
-    constructor(private readonly queryBus: QueryBus) { }
+    constructor(private readonly queryBus: QueryBus, private readonly commandBus: CommandBus) { }
+
+
+
+    @Post('add')
+    @HttpCode(201)
+    @UsePipes(new ValidationPipe({ transform: true }))
+    async createEmployee(@Body() newPerson: SavePersonCommand) {
+        return await this.commandBus.execute(newPerson);
+    }
 
     @Get('all')
     async getAll() {
